d590789804ba517ecb2737fe9ebe045e801211f7
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2022-02-24 23:18:35 +0700

    initial commit

---
+++ src/main/java/com/example/javaspringbootpractice/JavaSpringBootPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaspringbootpractice/JavaSpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
