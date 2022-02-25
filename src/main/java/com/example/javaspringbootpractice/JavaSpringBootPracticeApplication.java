68231b0859d0e23b491d6891ee2fa6abb34569ac
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2022-02-25 20:47:31 +0700

    feat(api): add user
    1.Create user entity
    2.Create user repository
    3.Initial user data
    
    Refs: #123

--- src/main/java/com/example/javaspringbootpractice/JavaSpringBootPracticeApplication.java
+++ src/main/java/com/example/javaspringbootpractice/JavaSpringBootPracticeApplication.java
@@ -1,10 +1,25 @@
 package com.example.javaspringbootpractice;
 
+import com.example.javaspringbootpractice.users.User;
+import com.example.javaspringbootpractice.users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class JavaSpringBootPracticeApplication {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@PostConstruct
+	public void initialUserData() {
+		User user = new User();
+		user.setName("kridsada");
+		userRepository.save(user);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringBootPracticeApplication.class, args);
