3ccb152175046aaeaeecd1aed098451f6f3f123b
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2022-02-26 20:01:43 +0700

    feat(api): add product to user cart #4
    Add cart request body
    
    Refs: #4

---
+++ src/main/java/com/example/javaspringbootpractice/carts/CartBody.java
@@ -0,0 +1,20 @@
+package com.example.javaspringbootpractice.carts;
+
+public class CartBody {
+
+    private int productId;
+    private int userId;
+    private int amount;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
